5e87321739ba23e97c2835aec07c04d4b6e53f2d
ltomm <ltomm@LAKEFLOWER>
2024-01-05 11:09:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace RemoteMultiMon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Count() > 0)
+                {
+                    var rdpFile = args[0];
+                    Console.WriteLine(rdpFile);
+                    var rdpContent = File.ReadAllText(rdpFile);
+                    if (rdpContent.IndexOf("use multimon") < 0)
+                    {
+                        using (var sw = new StreamWriter(rdpFile, true))
+                        {
+                            sw.WriteLine("\r\nuse multimon:i:1\r\n");
+                        }
+                    }
+
+                    Process.Start(@"C:\Windows\system32\mstsc.exe", $"{rdpFile}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+                throw;
+            }
+        }
+    }
+}
